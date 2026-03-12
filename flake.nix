963a8f7ec603c3d96de9556c27af36d522c1a78d
Wsesosse <achira363@gmail.com>
2026-03-12 15:55:04 +0700

    add srcg-axce

--- flake.nix
+++ flake.nix
@@ -1,17 +1,37 @@
 {
-    description = "srcg-core";
+  description = "srcg-core: cgroup wrapper for memory restricted execution";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs }: {
-        devShells.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-            packages = [
-                nixpkgs.legacyPackages.x86_64-linux.cmake
-                nixpkgs.legacyPackages.x86_64-linux.make
-                nixpkgs.legacyPackages.x86_64-linux.gcc
-            ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "srcg-core";
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.cmake ];
+          
+          installPhase = ''
+            mkdir -p $out/bin
+            cp srcg $out/bin/srcg
+          '';
         };
-    };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            gnumake
+            gcc
+          ];
+        };
+      }
+    );
 }
